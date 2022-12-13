1b853006d4f0e699c39cb09cf6d7ec4e3fe4cb0e
Aigul <sharipova.aig@gmail.com>
2022-12-12 22:31:57 -0500

    api

---
+++ script.js
@@ -0,0 +1,11 @@
+const APP = {
+  urls: {
+    base: 'https://swapi.dev/api/',
+    people: 'people/',
+    planets: 'planets/',
+    films: 'films/',
+    species: 'species/',
+    vehicles: 'vehicles/',
+    starships: 'starships/',
+  },
+}
